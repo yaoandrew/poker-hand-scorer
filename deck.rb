80e452766fb0d4c767744e63bb8182e0f505f8ca
yaoandrew <yaoandrew@gmail.com>
2017-04-07 14:53:39 -0700

    Makes file structure changes

---
+++ deck.rb
@@ -0,0 +1,16 @@
+# The deck should let us remove a card. It should tell us if a card is valid.
+# After removing a card, the card should no longer be valid.
+class Deck
+	def initialize
+		cards = []
+		suits = ["H" , "D", "S", "C"]
+		
+	end
+
+	def validate(card)
+	end
+	
+	def remove(card)
+	end
+
+end
