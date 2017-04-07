8d31c073dd838e67c636cb314f6571c2b754b80f
yaoandrew <yaoandrew@gmail.com>
2017-04-07 11:52:35 -0700

    Adds Rspec to project

--- poker.rb
+++ poker.rb
@@ -1,12 +1,15 @@
-class Card
-    attr_reader :suit, :value
+# TO DO - should there be one file per class?
 
-    def initialize(suit, value)
-        @suit = suit
-        @value = value
-    
+# Basic class to hold card attributes
+class Card
+	attr_reader :suit, :value
+	def initialize(suit, value)
+		@suit = suit
+		@value = value
 end
 
+# The deck should let us remove a card. It should tell us if a card is valid.
+# After removing a card, the card should no longer be valid.
 class Deck
-    
+	def initialize
 end
