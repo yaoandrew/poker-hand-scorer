e203a2174deb48bda5a1b24c8704042d63142b01
yaoandrew <yaoandrew@gmail.com>
2017-04-13 10:45:05 -0700

    Cleans up manual test file

--- test.rb
+++ test.rb
@@ -1,10 +1,10 @@
 require './deck'
 
 deck = Deck.new
-puts "Validation test"
-puts deck.validate(Card.new("H", 3))
-puts "Contains test"
-puts deck.contains(Card.new("H", 3))
-puts "REmove test"
-deck.remove(Card.new("H", 3))
-puts deck.contains(Card.new("H", 3))
+puts "Validation test- should be true"
+  puts deck.validate(Card.new("H", 3))
+puts "Contains test - should be true"
+  puts deck.contains(Card.new("H", 3))
+puts "Remove test - should be false"
+  deck.remove(Card.new("H", 3))
+  puts deck.contains(Card.new("H", 3))
