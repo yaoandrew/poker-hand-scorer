33fc9dc2ba40b2b02f171228c1d800988e8c5cc1
yaoandrew <yaoandrew@gmail.com>
2017-04-13 14:03:10 -0700

    Adds unit tests for all three methods in Deck

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,16 +1,33 @@
 require './deck'
+require './card'
 
-RSpec.describe Deck do
-  it { should_not be_empty}
+RSpec.describe Deck, "#contains_card?" do
+	it "returns if a card is in the deck when given a valid card" do
+		deck = Deck.new
+		expect(deck.contains_card?(Card.new("H", 3))).to eq true
+	end
 end
 
+RSpec.describe Deck, "#valid_card?" do
+		context "When the given card is valid" do 
+			it "returns true if a card is valid when given a valid card" do
+				deck = Deck.new
+				expect(deck.valid_card?(Card.new("C", 2))).to eq true
+			end
+		end
+		context "When a given card is not valid" do
+			it "returns false if a card is not valid when given an invalid card" do
+				deck = Deck.new
+				expect(deck.valid_card?(Card.new("X", 12))).to eq false
+			end
+		end
+end
 
-
-# Acceptance Criteria: Card should be able to be removed from Deck
-# Example Unit Test: - Given a deck with 10 cards
-# - When removeCard(someCard)
-# - Then, when getDeck() is called, someCard should not be in the array of cards returned
-
-# Given a deck with 20 cards
-# - When validateCard(someCard), it should return if someCard is a valid card
-# - methods - remove, validate, contains
+RSpec.describe Deck, "#remove" do
+	it "should not be in the deck after being removed" do
+		deck = Deck.new
+		card = Card.new("H", 1)
+		deck.remove(card)
+		expect(deck.contains_card?(card)).to eq false
+	end
+end
