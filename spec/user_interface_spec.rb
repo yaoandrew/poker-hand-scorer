ddb95baf834f4d01f3721abe3876268216138d51
yaoandrew <yaoandrew@gmail.com>
2017-04-21 10:57:04 -0700

    Move files into lib and clean up UI files

---
+++ spec/user_interface_spec.rb
@@ -0,0 +1,10 @@
+require_relative '../lib/user_interface'
+
+RSpec.describe UserInterface, "#prompt_user_for_card" do
+  it "prompts the user to enter a card" do
+    ui = UserInterface.new
+    prompt = "Please enter a valid card (Ex. H2 is the Two of Hearts):"
+    expect(STDOUT).to receive(:puts).with(prompt)
+    ui.prompt_user_for_card
+  end
+end
