4237bf3a5c543b0d2b9645456d8a7a3ccbaeccbf
yaoandrew <yaoandrew@gmail.com>
2017-05-11 17:13:06 -0700

    Add ability to detect straight in the hand

--- lib/hand.rb
+++ lib/hand.rb
@@ -18,8 +18,16 @@ class Hand
     @cards.max_by { |card| card.rank(Deck.FACE_RANKS) }
   end
 
+  def low_card
+    @cards.min_by { |card| card.rank(Deck.FACE_RANKS) }
+  end
+
+  def rank_difference
+    high_card.rank(Deck.FACE_RANKS) - low_card.rank(Deck.FACE_RANKS)
+  end
+
   def contains_pair?
-    card_occurences.has_value?(2)
+    card_occurences.value?(2)
   end
 
   def card_occurences
@@ -55,11 +63,15 @@ class Hand
   end
 
   def royal_sum
-    @cards.collect{ |card| card.rank(Deck.FACE_RANKS)}
-      .inject{|sum, card| sum + card}
+    @cards.collect { |card| card.rank(Deck.FACE_RANKS)}
+          .inject { |sum, card| sum + card}
   end
 
   def contains_royal_flush?
     royal_sum == 60 && contains_flush?
+  end
+
+  def contains_straight?
+    low_card.value == 2 && rank_difference == 12 || rank_difference == 4
   end
 end
