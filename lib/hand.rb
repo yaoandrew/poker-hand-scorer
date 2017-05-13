d33ae2a44a412b4151bb893338f1ca1ebae8e28c
yaoandrew <yaoandrew@gmail.com>
2017-05-12 17:11:04 -0700

    Fix contain_straight method

--- lib/hand.rb
+++ lib/hand.rb
@@ -18,22 +18,8 @@ class Hand
     @cards.max_by { |card| card.rank(Deck.FACE_RANKS) }
   end
 
-  def low_card
-    @cards.min_by { |card| card.rank(Deck.FACE_RANKS) }
-  end
-
-  def rank_difference
-    high_card.rank(Deck.FACE_RANKS) - low_card.rank(Deck.FACE_RANKS)
-  end
-
   def contains_pair?
     card_occurences.value?(2)
-  end
-
-  def card_occurences
-    hand = Hash.new(0)
-    @cards.each { |card| hand[card.value] += 1 }
-    hand
   end
 
   def contains_three_of_kind?
@@ -52,26 +38,46 @@ class Hand
     card_occurences.select { |_k, v| v == 2}.count == 2
   end
 
-  def suit_occurences
-    hand = Hash.new(0)
-    @cards.each { |card| hand[card.suit] += 1 }
-    hand
+  def contains_straight?
+    #low_card.value == 2 && rank_difference == 12 || rank_difference == 4
+    consecutive_range.compare(@cards.map { |card| card.rank(Deck.FACE_RANKS) })
   end
 
   def contains_flush?
     suit_occurences.value?(5)
   end
 
-  def royal_sum
-    @cards.collect { |card| card.rank(Deck.FACE_RANKS)}
-          .inject { |sum, card| sum + card}
-  end
-
   def contains_royal_flush?
     royal_sum == 60 && contains_flush?
   end
 
-  def contains_straight?
-    low_card.value == 2 && rank_difference == 12 || rank_difference == 4
+  def low_card
+    @cards.min_by { |card| card.rank(Deck.FACE_RANKS) }
+  end
+
+  def rank_difference
+    high_card.rank(Deck.FACE_RANKS) - low_card.rank(Deck.FACE_RANKS)
+  end
+
+  def consecutive_range
+    card_range = low_card.rank(Deck.FACE_RANKS)..high_card.rank(Deck.FACE_RANKS)
+    card_range.to_a
+  end
+
+  def card_occurences
+    hand = Hash.new(0)
+    @cards.each { |card| hand[card.value] += 1 }
+    hand
+  end
+
+  def suit_occurences
+    hand = Hash.new(0)
+    @cards.each { |card| hand[card.suit] += 1 }
+    hand
+  end
+
+  def royal_sum
+    @cards.collect { |card| card.rank(Deck.FACE_RANKS)}
+          .inject { |sum, card| sum + card}
   end
 end
