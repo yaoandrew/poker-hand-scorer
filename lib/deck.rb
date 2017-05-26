16c74541068849edd33aef8b4d6aed8f0704cafb
yaoandrew <yaoandrew@gmail.com>
2017-05-26 12:15:35 -0700

    Refactor class variable to constant inside class

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,16 +1,12 @@
 class Deck
-  @@VALUES = [0, 0].concat(Array(2..10)).concat(%w[J Q K A])
+  VALUES = [0, 0].concat(Array(2..10)).concat(%w[J Q K A])
 
   def initialize
     @cards = []
     @suits = %w[H D S C]
   end
 
-  def self.values
-    @@VALUES
-  end
-
   def valid_card?(card)
-    @suits.include?(card.suit) && @@VALUES.include?(card.value)
+    @suits.include?(card.suit) && VALUES.include?(card.value)
   end
 end
--- lib/hand.rb
+++ lib/hand.rb
@@ -1,23 +1,19 @@
 require 'deck'
 
 class Hand
-  @@HAND_TYPES = { RF: 'Royal Flush',
-                   SF: 'Straight Flush',
-                   FK: 'Four of a Kind',
-                   FH: 'Full House',
-                   FL: 'Flush',
-                   ST: 'Straight',
-                   TK: 'Three of a Kind',
-                   TP: 'Two Pair',
-                   PR: 'Pair',
-                   HC: 'High Card' }
+  HAND_TYPES = { RF: 'Royal Flush',
+                 SF: 'Straight Flush',
+                 FK: 'Four of a Kind',
+                 FH: 'Full House',
+                 FL: 'Flush',
+                 ST: 'Straight',
+                 TK: 'Three of a Kind',
+                 TP: 'Two Pair',
+                 PR: 'Pair',
+                 HC: 'High Card' }
 
   def initialize
     @cards = []
-  end
-
-  def HAND_TYPES
-    @@HAND_TYPES
   end
 
   def add_card(card)
@@ -29,19 +25,19 @@ class Hand
   end
 
   def high_card
-    @cards.max_by { |card| card.rank(Deck.values) }
+    @cards.max_by { |card| card.rank(Deck::VALUES) }
   end
 
   def low_card
-    @cards.min_by { |card| card.rank(Deck.values) }
+    @cards.min_by { |card| card.rank(Deck::VALUES) }
   end
 
   def rank_difference
-    high_card.rank(Deck.values) - low_card.rank(Deck.values)
+    high_card.rank(Deck::VALUES) - low_card.rank(Deck::VALUES)
   end
 
   def consecutive_sum
-    card_range = low_card.rank(Deck.values)..high_card.rank(Deck.values)
+    card_range = low_card.rank(Deck::VALUES)..high_card.rank(Deck::VALUES)
     card_range.reduce(:+)
   end
 
@@ -58,7 +54,7 @@ class Hand
   end
 
   def card_sum
-    @cards.collect { |card| card.rank(Deck.values)}
+    @cards.collect { |card| card.rank(Deck::VALUES)}
           .inject { |sum, card| sum + card}
   end
 
@@ -68,13 +64,13 @@ class Hand
 
   def royal_flush_sum
     royal_flush_card_values = [10, 'J', 'Q', 'K', 'A']
-    royal_flush_card_values.collect { |card_value| Deck.values.find_index card_value}
+    royal_flush_card_values.collect { |card_value| Deck::VALUES.find_index card_value}
                            .inject { |sum, card_value| sum + card_value }
   end
 
   def ace_low_straight_sum
     ace_low_values = ['A', 2, 3, 4, 5]
-    ace_low_values.collect { |card_value| Deck.values.find_index card_value}
+    ace_low_values.collect { |card_value| Deck::VALUES.find_index card_value}
                   .inject { |sum, card_value| sum + card_value }
   end
 end
