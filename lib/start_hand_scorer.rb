dd7c0bd78897907b80dcf65cfe969c47b6f3a3a6
yaoandrew <yaoandrew@gmail.com>
2017-05-26 16:40:36 -0700

    Prototyping game loop

--- lib/start_hand_scorer.rb
+++ lib/start_hand_scorer.rb
@@ -1,25 +1,52 @@
 #!/usr/local/bin/ruby
 
-require_relative './user_interface'
-require_relative './deck'
-require_relative './card'
-require_relative './hand'
+require_relative 'user_interface'
+require_relative 'deck'
+require_relative 'card'
+require_relative 'hand'
 
-ui = UserInterface.new
+
 # deck.valid_card?
 # hand.contains_card?
 # hand.add_card
 
-def parse_input(input_string)
-  suit = input_string[1]
-  value = input_string[0].to_i
-  card = Card.new(suit, value)
-  p deck.valid_card?(card)
-  hand.add_card(card)
-  p hand.inspect
+class CardCreator
+  require_relative 'card'
+  require_relative 'deck'
+  attr_accessor :suit, :value
+
+  def initialize(input_string)
+    @suit = input_string[1]
+    @value = input_string[0]
+
+    if @value.to_i.between?(2, 10)
+      @value = input_string[0].to_i
+    end
+  end
+
+  def create_card
+    Card.new(@suit, @value)
+  end
 end
 
-5.times do
-  ui.prompt_user_for_card
-  parse_input(gets.chomp)
+class HandCreator
+  require_relative 'hand'
+  hand = Hand.new
+  hand
 end
+
+ui = UserInterface.new
+ui.prompt_user_for_card
+input_string = gets.chomp
+creator = CardCreator.new(input_string)
+card = creator.create_card
+
+deck = Deck.new
+hand = Hand.new
+
+p deck.valid_card?(card)
+
+hand.add_card(card)
+
+p hand.inspect
+p card.suit
--- lib/user_interface.rb
+++ lib/user_interface.rb
@@ -2,4 +2,16 @@ class UserInterface
   def prompt_user_for_card
     puts 'Please enter a valid card (Ex. 2H is the Two of Hearts):'
   end
+
+  def card_exists
+    puts 'This card already exists in the hand'
+  end
+
+  def invalid_card
+    puts 'This is not a valid card'
+  end
+
+  def hand_results
+    puts 'Congrats, you have a...'
+  end
 end
