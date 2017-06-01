80054b91dad79b81c5d3e28f2f041764d467d859
yaoandrew <yaoandrew@gmail.com>
2017-06-01 11:54:35 -0700

    Fix broken test to STDOUT

--- spec/user_interface_spec.rb
+++ spec/user_interface_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe UserInterface, '#prompt_user_for_card' do
   it 'prompts the user to enter a card' do
     ui = UserInterface.new
     prompt = 'Please enter a valid card (Ex. 2H is the Two of Hearts):'
-    expect(STDOUT).to receive(:puts).with(prompt)
-    ui.prompt_user_for_card
+    expect{ui.prompt_user_for_card}.to output(prompt).to_stdout
   end
 end
