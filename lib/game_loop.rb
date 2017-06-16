104718a1cdf9d7999bbc43f1585c686ce607826c
yaoandrew <yaoandrew@gmail.com>
2017-06-15 18:35:19 -0700

    Create new class to extract game loop logic

---
+++ lib/game_loop.rb
@@ -0,0 +1,2 @@
+class GameLoop
+end
---
+++ spec/game_loop_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../lib/deck'
+require_relative '../lib/hand'
+require_relative '../lib/card'
+require_relative '../lib/game_loop'
+
+RSpec.describe GameLoop, '#start' do
+  context 'When the game loop is called' do
+    it 'should call the start method' do
+    end
+  end
+end
